132f51765f38f362e631bb7b493a624238f7489a
Valentine0604 <ewecia.s@gmail.com>
2024-05-13 13:44:11 +0200

    Update

--- App.js
+++ App.js
@@ -1,57 +1,63 @@
 const http = require('http');
 const os = require('os');
 
-// Funkcja do uzyskiwania informacji o serwerze
-function getServerInfo() {
-    const authorName = "Ewelina Salata";
-    const port = process.env.PORT || 3000;
-    const serverStartTime = new Date().toLocaleString();
-    const serverHostname = os.hostname();
-    return {
-        authorName,
-        port,
-        serverStartTime,
-        serverHostname
-    };
-}
+const PORT = 3000;
+const authorName = "Ewelina Salata";
 
-// Funkcja obsługująca żądania HTTP
-function requestHandler(req, res) {
-    // Pobranie informacji o serwerze
-    const serverInfo = getServerInfo();
-
-    // Pobranie adresu IP klienta
-    const clientIp = req.connection.remoteAddress;
-
-    // Pobranie daty i czasu w strefie czasowej klienta
-    const clientDateTime = new Date().toLocaleString();
-
-    // Wyświetlenie informacji o kliencie w konsoli serwera
-    console.log(`Adres IP klienta: ${clientIp}`);
-
-    // Wyświetlenie informacji o kliencie w przeglądarce
+const server = http.createServer((req, res) => {
+    // Pobieranie informacji o adresie IP klienta
+    const clientIP = req.socket.remoteAddress;
+    
+    // Pobieranie daty i godziny w strefie czasowej klienta
+    const currentTime = new Date().toLocaleString('pl-PL', {timeZone: 'Europe/Warsaw'});
+    
+    // Pobieranie daty i czasu uruchomienia serwera
+    const serverStartTime = new Date().toLocaleString('pl-PL', {timeZone: 'Europe/Warsaw'});
+    
+    // Pobieranie adresu serwera
+    const serverAddress = os.hostname();
+    
+    // Zapisywanie informacji w logach serwera
+    console.log(`Serwer uruchomiony przez: ${authorName} na porcie: ${PORT}`);
+    console.log(`Adres IP klienta: ${clientIP}`);
+    console.log(`Data i godzina w strefie czasowej klienta: ${currentTime}`);
+    console.log(`Data i godzina uruchomienia serwera: ${serverStartTime}`);
+    console.log(`Adres serwera: ${serverAddress}`);
+    
+    // Tworzenie treści strony dla klienta
+    const responseBody = `
+        <html>
+            <head>
+                <title>Informacje o kliencie</title>
+            </head>
+            <body>
+                <h1>Informacje o kliencie</h1>
+                <p>Adres IP klienta: ${clientIP}</p>
+                <p>Data i godzina w twojej strefie czasowej: ${currentTime}</p>
+                
+                <h1>Informacje o serwerze</h1>
+                <ul>
+                  <li>Autor serwera: ${authorName}</li>
+                  <li>Port serwera: ${PORT}</li>
+                  <li>Data i godzina uruchomienia serwera: ${serverStartTime}</li>
+                  <li>Adres serwera: ${serverAddress}</li>
+                </ul>
+            </body>
+        </html>
+    `;
+    
+    // Ustawienie nagłówków odpowiedzi HTTP
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(`<html><head><title>Informacje o kliencie</title></head><body>`);
-    res.write(`<h1>Informacje o kliencie</h1>`);
-    res.write(`<p>Adres IP klienta: ${clientIp}</p>`);
-    res.write(`<p>Data i czas w strefie czasowej klienta: ${clientDateTime}</p>`);
-    res.write(`<p>Informacje o serwerze:</p>`);
-    res.write(`<ul>`);
-    res.write(`<li>Autor serwera: ${serverInfo.authorName}</li>`);
-    res.write(`<li>Adres serwera: ${serverInfo.serverHostname}</li>`);
-    res.write(`<li>Port serwera: ${serverInfo.port}</li>`);
-    res.write(`<li>Data i czas uruchomienia serwera: ${serverInfo.serverStartTime}</li>`);
-    res.write(`</ul>`);
-    res.write(`</body></html>`);
-    res.end();
-}
+    
+    // Wysłanie odpowiedzi do klienta
+    res.end(responseBody);
+});
 
-// Tworzenie serwera HTTP
-const server = http.createServer(requestHandler);
+server.on('error', (err) => {
+    console.error('Wystąpił błąd serwera:', err.message);
+});
 
-// Nasłuchiwanie na określonym porcie
-const port = process.env.PORT || 3000;
-const authorName = "Ewelina Sałata";
-server.listen(port, () => {
-    console.log(`Serwer uruchomiony. Port: ${port}. Autor: ${authorName}.`);
+server.listen(PORT, () => {
+    // Wyświetlanie informacji o uruchomieniu serwera
+    console.log(`Serwer uruchomiony na porcie ${PORT} przez ${authorName} o godzinie ${new Date().toLocaleString('pl-PL', {timeZone: 'Europe/Warsaw'})}`);
 });
--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,50 @@
 # Etap 1: Budowanie aplikacji
-FROM node:alpine AS builder
+FROM scratch as builder
 
-WORKDIR /home/node/app
+# Dodanie zawartości systemu plików Alpine Linux jako bazowego obrazu
+ADD alpine-minirootfs-3.19.1-aarch64.tar /
 
-COPY App.js package.json ./
+# Aktualizacja pakietów, instalacja Node.js i npm, czyszczenie cache
+RUN apk update && apk upgrade && apk add --no-cache nodejs=20.12.1-r0 npm=10.2.5-r0 && rm -rf /etc/apk/cache 
 
-RUN npm install --only=production
+# Dodanie użytkownika i grupy 'node' oraz przejście do kontekstu użytkownika 'node'
+RUN addgroup -S node && adduser -S node -G node 
+USER node 
+
+# Ustawienie katalogu roboczego
+WORKDIR /home/node/app 
+
+# Skopiowanie plików aplikacji oraz pliku package.json
+COPY --chown=node:node App.js ./App.js
+COPY --chown=node:node package.json ./package.json
+
+# Instalacja zależności aplikacji
+RUN npm install 
 
 # Etap 2: Uruchomienie aplikacji w środowisku produkcyjnym
-FROM node:alpine
+FROM node:22-alpine
 
-LABEL maintainer="Ewelina Sałata"
-
+# Ustawienie zmiennej środowiskowej VERSION lub domyślnej wartości v1.0.0
 ARG VERSION
 ENV VERSION=${VERSION:-v1.0.0}
 
+# Instalacja Curl do obsługi healthchecka
 RUN apk add --update --no-cache curl
 
+# Ustawienie kontekstu użytkownika 'node' i utworzenie katalogu dla aplikacji
+USER node
+RUN mkdir -p /home/node/app
 WORKDIR /home/node/app
 
-COPY --from=builder /home/node/app .
+# Skopiowanie plików aplikacji z poprzedniego etapu
+COPY --from=builder --chown=node:node /home/node/app ./
 
-USER node
-
+# Odsłuch na porcie 3000
 EXPOSE 3000
 
-HEALTHCHECK --interval=30s --timeout=10s --start-period=5s --retries=3 \
+# Ustawienie healthchecka
+HEALTHCHECK --interval=4s --timeout=20s --start-period=2s --retries=3 \
     CMD curl -f http://localhost:3000/ || exit 1
 
-CMD ["node", "App.js"]
+# Uruchomienie aplikacji
+ENTRYPOINT ["node", "App.js"]
